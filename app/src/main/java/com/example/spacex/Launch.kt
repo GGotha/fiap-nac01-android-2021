fa15af685f150b2e90989e678909e3e6dff2440c
ggotha <snakygamer@gmail.com>
2021-09-13 16:25:17 -0300

    add readme

--- app/src/main/java/com/example/spacex/Launch.kt
+++ app/src/main/java/com/example/spacex/Launch.kt
@@ -1,15 +1,7 @@
 package com.example.spacex
 
 
-import com.google.gson.annotations.SerializedName
-
 data class Launch(
     val id: String,
     val name: String?,
-//    val links: String?
 )
-
-//data class Image(
-//    @SerializedName("net") val image: Boolean?
-//)
-
